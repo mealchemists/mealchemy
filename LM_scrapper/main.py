205a647688e589fc6d843574aed9fd2358f7d4bf
logan <myemil@email.com>
2025-01-21 17:46:16 -0700

    updates

--- LM_scrapper/main.py
+++ LM_scrapper/main.py
@@ -1,18 +1,18 @@
-from scrapper import scrape_website, split_dom_content, clean_body_content, extract_body_content
+from scrapper import Scraper
 # from parse import parse_with_ollame
 import sys
 
 url = "https://www.allrecipes.com/recipe/228823/quick-beef-stir-fry/"
+url = "https://www.simplyrecipes.com/million-dollar-ravioli-casserole-recipe-8774485"
 # url = "https://pinchofyum.com/vegan-crunchwrap"
 # url = "https://techwithtim.net"
 if len(sys.argv) > 1:
     url = sys.argv[1]
     
-result = scrape_website(url)
-# print(result)
-body_content = extract_body_content(result)
-# print(body_content)
-cleaned_content = clean_body_content(body_content)
+scraper = Scraper(url)
+result = scraper.scrape_website(url)
+body_content = scraper.extract_body_content(result)
+cleaned_content = scraper.clean_body_content(body_content)
 print(cleaned_content)
 
 # dom_chunks = split_dom_content(cleaned_content)
--- LM_scrapper/scrapper.py
+++ LM_scrapper/scrapper.py
@@ -2,52 +2,75 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
+from lookup import lookup_data
+from util import extract_base_domain
+class Scraper:
+    def __init__(self, url):
+        self.url = url
+        self.lookupdata = lookup_data
 
-def scrape_website(website):
-    print("Launching Chrome...")
+        if url:
+            self.base_domain = extract_base_domain(url)
+
     
-    # Automatically download and install the correct ChromeDriver
-    chrome_driver_path = ChromeDriverManager().install()
-    
-    # Set up Chrome options if needed
-    options = webdriver.ChromeOptions()
-    
-    # Launch the Chrome browser with the automatically downloaded ChromeDriver
-    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-    
-    try:
-        driver.get(website)
-        print("Page loaded")
-        html = driver.page_source
-        return html
-    finally:
-        driver.quit()
+    def scrape_website(self, website):
+        print("Launching Chrome...")
         
-def extract_body_content(html_content):
-    soup = BeautifulSoup(html_content, "html.parser")
-    body_content = soup.find("ul", attrs={"class": "mm-recipes-structured-ingredients__list"})
-    step_content = soup.find("div", attrs={"id": "mm-recipes-steps_1-0"})
-    recipe_details = soup.find("div", attrs={"id": "mm-recipes-details_1-0"})
-    nutrition_details = soup.find("div", attrs={"id": "mm-recipes-nutrition-facts-summary_1-0"})
-    if body_content:
-        return str("ingredients\n" + body_content.text + "steps\n" + step_content.text + "recipe_details" + recipe_details + "nutrition" +nutrition_details)
-    return ""
-
-def clean_body_content(body_content):
-    soup = BeautifulSoup(body_content, "html.parser")
-    
-    for script_or_style in soup(['script','style']):
-        script_or_style.extract()
+        # Automatically download and install the correct ChromeDriver
+        chrome_driver_path = ChromeDriverManager().install()
         
-    cleaned_content = soup.get_text(separator="\n")
-    cleaned_content = "\n".join(
-        line.strip() for line in cleaned_content.splitlines() if line.strip()
-    )
-    
-    return cleaned_content
+        # Set up Chrome options if needed
+        options = webdriver.ChromeOptions()
+        
+        # Launch the Chrome browser with the automatically downloaded ChromeDriver
+        driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+        
+        try:
+            driver.get(website)
+            print("Page loaded")
+            html = driver.page_source
+            return html
+        finally:
+            driver.quit()
+            
+    def extract_body_content(self, html_content):
+        soup = BeautifulSoup(html_content, "html.parser")
 
-def split_dom_content(dom_content, max_length=6000):
-    return[
-        dom_content[i: 1+max_length] for i in range(0, len(dom_content), max_length)
-    ]
-    
+        website_lookup = lookup_data[self.base_domain]
+
+        ingredient = website_lookup['ingredient']  
+        recipe_step = website_lookup['recipe_step']  
+        recipe_detail = website_lookup['recipe_details']  
+        nutrition_detail = website_lookup['nutrition_details'] 
+
+        ingredient_tags = soup.find(ingredient['tag'], attrs=ingredient['attrs'])
+        recipe_step_tags = soup.find(recipe_step['tag'], attrs=recipe_step['attrs'])
+        recipe_detail_tags = soup.find(recipe_detail['tag'], attrs=recipe_detail['attrs'])
+        nutrition_detail_tags = soup.find(nutrition_detail['tag'], attrs=nutrition_detail['attrs'])
+
+        # body_content = soup.find("ul", attrs={"class": "mm-recipes-structured-ingredients__list"})
+        # step_content = soup.find("div", attrs={"id": "mm-recipes-steps_1-0"})
+        # recipe_details = soup.find("div", attrs={"id": "mm-recipes-details_1-0"})
+        # nutrition_details = soup.find("div", attrs={"id": "mm-recipes-nutrition-facts-summary_1-0"})
+        if ingredient_tags:
+            return str("\n\ningredients\n" + ingredient_tags.text + "\n\nsteps\n" + recipe_step_tags.text + "\n\nrecipe_details\n" + recipe_detail_tags.text + "\n\nnutrition\n" + nutrition_detail_tags.text)
+        return ""
+
+    def clean_body_content(self, body_content):
+        soup = BeautifulSoup(body_content, "html.parser")
+        
+        for script_or_style in soup(['script','style']):
+            script_or_style.extract()
+            
+        cleaned_content = soup.get_text(separator="\n")
+        cleaned_content = "\n".join(
+            line.strip() for line in cleaned_content.splitlines() if line.strip()
+        )
+        
+        return cleaned_content
+
+    def split_dom_content(self, dom_content, max_length=6000):
+        return[
+            dom_content[i: 1+max_length] for i in range(0, len(dom_content), max_length)
+        ]
+        
