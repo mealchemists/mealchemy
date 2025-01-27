7c6dc5e8cf1cffeb72df136fc52ec9df8526f4e0
xirui1219 <crystal7@ualberta.ca>
2025-01-26 21:53:51 -0700

    chose calendar

--- frontend/src/Components/MealPlanningPage/MealPlanningPage.tsx
+++ frontend/src/Components/MealPlanningPage/MealPlanningPage.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import './MealPlanningPage.css'
+
+const localizer = momentLocalizer(moment)
+const events = [
+  {
+    start:moment('2025-01-26T18:00:00').toDate(),
+    end:moment('2025-01-26T20:00:00').toDate(),
+    title:"Test"
+  }
+]
+
+const CustomToolbar = ({ label, onNavigate }) => (
+  <div className="rbc-toolbar">
+    <div className="rbc-toolbar-label">{label}</div>
+    <div className="rbc-btn-group rbc-btn-group-left">
+      <button type="button" onClick={() => onNavigate('TODAY')}>Today</button>
+      <button type="button" onClick={() => onNavigate('PREVIOUS')}>Back</button>
+      <button type="button" onClick={() => onNavigate('NEXT')}>Next</button>
+    </div>
+    <div className="rbc-btn-group rbc-btn-group-right">
+      <button type="button" className="add-to-shopping-list-btn">Add to Shopping List</button>
+    </div>
+  </div>
+);
 
 function MealPlanningPage() {
-    return (
-       <h1>MealPlanningPage</h1>
-    );
+    const [myEventsList, setMyEventsList] = useState([])
+return (<div className='calendarContainer'>
+    <Calendar
+      localizer={localizer}
+      events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      defaultView="week"
+      min={new Date(2023, 1, 1, 0, 0)} 
+      max={new Date(2023, 1, 1, 23, 59)}
+      views={{ week: true }}
+      components={{
+        toolbar: CustomToolbar, // Apply custom toolbar
+      }}
+      style={{ height: '500px', width:'800px'}}
+          />
+  </div>)
 }
 
 export default MealPlanningPage;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { HashRouter } from "react-router-dom";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
