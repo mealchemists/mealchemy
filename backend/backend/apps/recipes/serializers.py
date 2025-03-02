374fc09c1433c8c1b3866d3941211f40143575b7
kosedy <kosedy@ualberta.ca>
2025-03-01 19:37:42 -0700

    post working

--- backend/backend/apps/recipes/serializers.py
+++ backend/backend/apps/recipes/serializers.py
@@ -7,6 +7,20 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = '__all__'
         
+    def create(self, validated_data):
+        """
+        Create a recipe and its ingredients in a single request.
+        """
+        ingredients_data = validated_data.pop("ingredients")
+        recipe = Recipe.objects.create(**validated_data)
+
+        # Create RecipeIngredient instances
+        for ingredient_data in ingredients_data:
+            ingredient_data["recipe"] = recipe
+            self.fields["ingredients"].create(ingredient_data)
+
+        return recipe
+        
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
@@ -15,9 +29,13 @@ class IngredientSerializer(serializers.ModelSerializer):
 class RecipeIngredientSerializer(serializers.ModelSerializer):
     recipe = RecipeSerializer()
     # ingredient = IngredientSerializer(many=True)
-
+    
     class Meta:
         model = RecipeIngredient
         fields = '__all__'
+        
+    
+        
+
     
 
