d999b325e95d24b409f2f4a3e3a1303ca652b0be
kosedy <kosedy@ualberta.ca>
2025-03-23 18:57:59 -0600

    All user auth

--- backend/backend/apps/user/serializer.py
+++ backend/backend/apps/user/serializer.py
@@ -7,11 +7,12 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ['email', 'password']
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            username=validated_data['username'],
+            username=validated_data['email'],
+            email=validated_data['email'],
             password=validated_data['password']
         )
         return user
