3922383249ad50980baa3f1b706bd2d19ee15475
echeng-git <eccheng@ualberta.ca>
2025-03-25 15:54:40 -0600

    integrations as well

--- recipe-extractors/pdf/main.py
+++ recipe-extractors/pdf/main.py
@@ -1,4 +1,4 @@
-from pdf_utils import PDFUtils
+from .pdf_utils import PDFUtils
 import requests
 
 django_url = "http://localhost:8000/api/recipe-ingredients"
@@ -7,8 +7,7 @@ django_url = "http://localhost:8000/api/recipe-ingredients"
 def extract_recipe_data_pdf(rest_url, user, token):
     # get PDF data from server
     # NOTE: temp hardcoded path for now
-    print(rest_url)
-    TEMP_PATH = "./source_material/hardcopy_scans/multi/Healthy Family Week1.pdf"
+    TEMP_PATH = "./pdf/source_material/hardcopy_scans/multi/Healthy Family Week1.pdf"
 
     # load and extract
     pages = PDFUtils.load_pdf_pages_path(TEMP_PATH)
@@ -17,4 +16,19 @@ def extract_recipe_data_pdf(rest_url, user, token):
     # LLM?
 
     # post to server
+    # result["recipe"]["source_url"] = url
+    # result["recipe"]["user"] = user
+    #
+    # headers = {
+    #     "Authorization": f"Bearer {token}",  # Add the token in Authorization header
+    # }
+    # response = requests.post(url=django_url, json=result, headers=headers)
+    #
+    # # Check if the request was successful
+    # if response.status_code == 201:
+    #     print("Successfully sent the recipe data.")
+    # else:
+    #     print(
+    #         f"Failed to send recipe data. Status Code: {response.status_code}, Response: {response.text}"
+    #     )
     return
