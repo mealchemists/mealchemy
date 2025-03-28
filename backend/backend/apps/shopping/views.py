34ea68b6a72210c2a79da986948b26fe300705db
xirui1219 <crystal7@ualberta.ca>
2025-03-27 21:39:13 -0600

    made shopping_list as well

--- backend/backend/apps/shopping/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from .models import Recipe
-from django.shortcuts import render
-
-# Create your views here.
-# TODO: Determine the endpoints.
-class ShoppingViewSet(viewsets.ViewSet):
-    pass
