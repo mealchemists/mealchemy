d0e0ccbe6a189cfabf689f5bda422d63c1121f58
echeng-git <eccheng@ualberta.ca>
2025-03-31 19:45:10 -0600

    Try to ignore if we are testing

--- backend/backend/apps/recipes/producer.py
+++ backend/backend/apps/recipes/producer.py
@@ -6,11 +6,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-params = pika.URLParameters(os.environ.get("PIKA_URL", "amqp://localhost:5672/"))
-connection = pika.BlockingConnection(params)
-channel = connection.channel()
+is_testing = os.environ.get("DJANGO_TEST", "FALSE").upper() == "TRUE"
+
+if not is_testing:
+    try:
+        params = pika.URLParameters(
+            os.environ.get("PIKA_URL", "amqp://localhost:5672/")
+        )
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+    except Exception as _:
+        channel = None
 
 
 def publish(data):
     body = json.dumps(data).encode("utf-8")  # Serialize to JSON and convert to bytes
-    channel.basic_publish(exchange="", routing_key="admin", body=body)
+    if channel is not None:
+        try:
+            channel.basic_publish(exchange="", routing_key="admin", body=body)
+        except Exception as e:
+            print(e)
