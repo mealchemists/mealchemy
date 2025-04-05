fde0e47c73daebc5fab9bd97162423a552a9ca72
kosedy <kosedy@ualberta.ca>
2025-04-05 14:09:57 -0600

    final update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -264,11 +264,12 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CORS_ALLOW_CREDENTIALS = True
 
-SESSION_COOKIE_SAMESITE = "None"  # Allow cross-site cookies
-SESSION_COOKIE_SECURE = True  # Only send cookies over HTTPS
-CSRF_COOKIE_SAMESITE = "None"  # CSRF cookies for cross-site requests
-CSRF_COOKIE_SECURE = True  # Only send cookies over HTTPS
-CSRF_COOKIE_DOMAIN = "mealchemy.app"
+if os.getenv("PROD", "False").lower() == "true":
+    SESSION_COOKIE_SAMESITE = "None"  # Allow cross-site cookies
+    SESSION_COOKIE_SECURE = True  # Only send cookies over HTTPS
+    CSRF_COOKIE_SAMESITE = "None"  # CSRF cookies for cross-site requests
+    CSRF_COOKIE_SECURE = True  # Only send cookies over HTTPS
+    CSRF_COOKIE_DOMAIN = "mealchemy.app"
 
 print(f" allowed {ALLOWED_HOSTS}")
 print(f" trusted_origins {CSRF_TRUSTED_ORIGINS}")
