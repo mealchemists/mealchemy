63fd43afa2e3e32be0eac0eff7e761f85ee98fd2
kosedy <kosedy@ualberta.ca>
2025-04-05 21:59:36 -0600

    Revert "Merge pull request #166 from mealchemists/revert-165-dev-recipe-review"
    This reverts commit e9599286ed5b6efeadd1862022a44a1c9aa7c5a8, reversing
    changes made to 591f4044ef60168b8307417679faac9970eb025f.

--- backend/backend/apps/recipes/apps.py
+++ backend/backend/apps/recipes/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def my_callback(sender, **kwargs):
+    from django.apps import apps
+    Aisle = apps.get_model('recipes', 'Aisle')
+    Aisle.objects.get_or_create(name="Uncategorized")
 
 class RecipesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend.apps.recipes'
+    
+    def ready(self):
+        post_migrate.connect(my_callback, sender=self)
